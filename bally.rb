82cfffb9b8ce995d8eb95afd2d18b8c4d6526e77
Karel Vervaeke <karel@outerthought.org>
2011-12-11 18:31:31 +0100

    WIP

--- bally.rb
+++ bally.rb
@@ -1,13 +1,31 @@
 require 'gtk2'
 
 class GameContext
-  attr_accessor :images,:tiles,:height,:width
+  attr_accessor :images,:tiles,:height,:width,:steps,:pb,:drawingarea,:gc
 
-  def initialize()
+  def initialize(window)
+    @window=window
     @images={}
-    @tiles=[10,10]
-    @height=600
+
+    @steps=25
+    puts "Preloading images"
+    (0..@steps).each { |i|
+      name="xspin%02d"%i
+      images[name] = Gdk::Pixbuf.new("images/#{name}.png")
+      name="yspin%02d"%i
+      images[name] = Gdk::Pixbuf.new("images/#{name}.png")
+    }
+
     @width=800
+    @height=600
+    @gridwidth=10
+    @gridheight=10
+
+    @drawingarea=Gtk::DrawingArea.new
+    window.add(@drawingarea)
+    @drawingarea.set_size_request(400, 400)
+    @gc=Gdk::GC.new(@drawingarea.window)
+
   end
 
   def x_offset()
@@ -20,12 +38,16 @@ class GameContext
   end
 
   def gridpos(x,y)
-    return [ x_offset + (0.5 + x) * (@width / 10), y_offset + (0.5 + y) * (@height / 10) ]
+    return [ x_offset + (0.5 + x) * @width / @gridwidth, y_offset + (0.5 + y) * @height / @gridwidth ]
   end
   def gridsize
     @width > @height ? @height : @width
   end
   
+  def draw_grid()
+    @gc.foreground=Gdk::Color.new(0,0,0)
+    @drawingarea.window.draw_rectangle(gc, false, x_offset, y_offset, gridsize, gridsize)
+  end
 end
 
 class Bally
@@ -33,38 +55,30 @@ class Bally
   end
 
   def start
-    @ctx = GameContext.new
-    puts "Preloading images"
-    (0..10).each { |i|
-      name="xspin%02d"%i
-      @ctx.images[name] = Gdk::Pixbuf.new("images/#{name}.png")
-      name="yspin%02d"%i
-      @ctx.images[name] = Gdk::Pixbuf.new("images/#{name}.png")
-    }
     window=Gtk::Window.new
+    window.set_title("Bally")
+    window.border_width=10
+    window.show_all
+    @ctx = GameContext.new(window)
+    window.set_size_request(@ctx.width, @ctx.height)
+
+
     window.signal_connect("destroy") {
       Gtk.main_quit
     }
 
-    window.set_title("Bally")
-    window.border_width=10
-    window.show_all
-    window.set_size_request(@ctx.width, @ctx.height)
-
     @balls=[]
-    @drawingarea=Gtk::DrawingArea.new
-    @drawingarea.set_size_request(400, 400)
-    @drawingarea.add_events(Gdk::Event::BUTTON_PRESS_MASK)
-    @drawingarea.signal_connect("button-press-event") { |e,d|
-      @balls << Ball.new(0,5)
+
+    area=@ctx.drawingarea
+    area.add_events(Gdk::Event::BUTTON_PRESS_MASK)
+    area.signal_connect("button-press-event") { |e,d|
+      @balls << Ball.new(0,5,@ctx)
     }
     
-    window.add(@drawingarea)
-
     @grid=[]
     10.times { @grid << Array.new(10) }
 
-    Gtk.timeout_add(10) {
+    Gtk.timeout_add(30) {
       update_positions()
       update_graphics()
       true
@@ -75,54 +89,52 @@ class Bally
   end
 
   def update_positions
-    @balls.each { |ball| ball.update }
+    @balls.each { |ball| ball.update @ctx }
     @balls.delete_if { |ball| not ball.keep? }
   end
 
   def update_graphics
-    gc=Gdk::GC.new(@drawingarea.window)
-    gc.foreground=Gdk::Color.new(0,0,0)
-    @drawingarea.window.draw_rectangle(gc, false, @ctx.x_offset, @ctx.y_offset, @ctx.gridsize, @ctx.gridsize)
-    gc.foreground=Gdk::Color.new(255,255,255)
+    @ctx.draw_grid()
     @balls.each_with_index { |ball,idx|
-      ball.draw(@ctx,gc,@drawingarea.window)
+      ball.draw @ctx
     }
-  end
-  def tell_time
-    puts "the time is ..."
   end
 end
 
 class Ball
   attr_accessor :x,:y
 
-  def initialize(x,y)
+  def initialize(x,y,ctx)
     @x=x
     @y=y
     @direction=[1,0]
     @step=0
     @kleur=0
+
+    puts "x = #{x}"
+    puts "y = #{y}"
   end
   
-  def draw(ctx, gc, drawable)
+  def draw(ctx)
     gridpos1=ctx.gridpos(@x, @y)
     gridpos2=ctx.gridpos(@x + @direction[0], @y + @direction[1])
-    @direction == [1,0] and imgname="yspin%02d"%(@step)
-    @direction == [-1,0] and imgname="yspin%02d"%(10-@step)
-    @direction == [0,1] and imgname="xspin%02d"%(@step)
-    @direction == [0,-1] and imgname="xspin%02d"%(10-@step)
+    @direction == [1,0] and imgname="yspin%02d"%(ctx.steps-@step)
+    @direction == [-1,0] and imgname="yspin%02d"%(@step)
+    @direction == [0,1] and imgname="xspin%02d"%(ctx.steps-@step)
+    @direction == [0,-1] and imgname="xspin%02d"%(@step)
     pb=ctx.images[imgname]
-    gx = gridpos1[0] + (gridpos2[0]-gridpos1[0]) * @step/10 - pb.width/2
-    gy = gridpos1[1] + (gridpos2[1]-gridpos1[1]) * @step/10 - pb.height/2
-    drawable.draw_pixbuf(gc, pb, 0, 0, gx, gy, -1, -1, Gdk::RGB::DITHER_NONE, -1, -1)
+    gx = gridpos1[0] + (gridpos2[0]-gridpos1[0]) * @step/ctx.steps - pb.width/2
+    gy = gridpos1[1] + (gridpos2[1]-gridpos1[1]) * @step/ctx.steps - pb.height/2
+    ctx.drawingarea.window.draw_pixbuf(ctx.gc, pb, 0, 0, gx, gy, -1, -1, Gdk::RGB::DITHER_NONE, -1, -1)
   end
 
-  def update()
+  def update(ctx)
     @step+=1
-    if @step>=10
+    if @step>=ctx.steps
       @step=0
       @x+=@direction[0]
       @y+=@direction[1]
+      puts "new ball position is #{@x},#{@y}"
       #TODO:if the ball is on an arrow tile,
       #change @direction
     end
