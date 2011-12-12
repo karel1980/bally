4f27012eddafa65a92638f00622459cda73cd6a9
Karel Vervaeke <karel@outerthought.org>
2011-12-12 22:26:31 +0100

    Implemented direction changing

--- bally.rb
+++ bally.rb
@@ -1,13 +1,20 @@
 require 'gtk2'
 
 class Bally
-  attr_accessor :images,:tiles,:height,:width,:steps,:pb,:drawingarea,:gc,:gridwidth,:gridheight
+  attr_accessor :images,:tiles,:height,:width,:steps,:pb,:drawingarea,:gc,:gridwidth,:gridheight,:grid
 
   def initialize()
     @width=800
     @height=600
     @gridwidth=10
     @gridheight=10
+    @expired_balls=[]
+
+    @grid=[]
+    @gridwidth.times { @grid << Array.new(gridheight) }
+    @grid[3][5]=[0,-1]
+    @grid[3][2]=[1,0]
+    @grid[6][2]=[0,1]
 
     @images={}
     @steps=25
@@ -35,9 +42,10 @@ class Bally
 
     @drawingarea=Gtk::DrawingArea.new
     window.add(@drawingarea)
-    @drawingarea.set_size_request(width, height)
+    @drawingarea.set_size_request(levelsize[0], levelsize[1])
     @gc=Gdk::GC.new(@drawingarea.window)
 
+    puts x_offset, y_offset
   end
 
   def x_offset()
@@ -49,21 +57,31 @@ class Bally
     return (@height - @width) / 2
   end
 
-  def gridpos(x,y)
-    return [ x_offset + (0.5 + x) * @width / @gridwidth, y_offset + (0.5 + y) * @height / @gridwidth ]
+  def gridcenter(x,y)
+    return [ x_offset + (0.5 + x) * levelsize[0] / @gridwidth, y_offset + (0.5 + y) * levelsize[1] / @gridheight ]
   end
-  def gridsize
-    @width > @height ? @height : @width
-  end
-  
+
   def draw_grid()
     @gc.foreground=Gdk::Color.new(0,0,0)
+    w,h = levelsize
+    # Draw vertical lines
     (0..@gridwidth).each { |i|
-      @drawingarea.window.draw_line(gc, x_offset + i*width/gridwidth, y_offset, x_offset + i*width/gridwidth, y_offset+height)
+      @drawingarea.window.draw_line(gc, x_offset + i*w/gridwidth, y_offset, x_offset + i*w/gridwidth, y_offset+h)
     }
+    # Draw horizontal lines
     (0..@gridheight).each { |i|
-      @drawingarea.window.draw_line(gc, x_offset, y_offset + i*height/gridheight, x_offset + width/gridheight, y_offset+ + i*height/gridheight)
+      @drawingarea.window.draw_line(gc, x_offset, y_offset + i*h/gridheight, x_offset + w, y_offset+ + i*h/gridheight)
     }
+  end
+
+  # returns a multiple of the grid's size
+  # which fits snugly in the available drawingarea
+  def levelsize
+    if (gridwidth * width > gridheight * height)
+      return [width, width * gridheight / gridwidth]
+    else
+      return [height * gridwidth / gridheight, height]
+    end
   end
 
   def start
@@ -76,9 +94,6 @@ class Bally
       @balls << Ball.new(0,5,self)
     }
     
-    @grid=[]
-    10.times { @grid << Array.new(10) }
-
     Gtk.timeout_add(30) {
       update_positions()
       update_graphics()
@@ -91,14 +106,21 @@ class Bally
 
   def update_positions
     @balls.each { |ball| ball.update self }
-    @balls.delete_if { |ball| not ball.keep? }
+    @balls -= @expired_balls
+    @expired_balls.clear
   end
 
   def update_graphics
+    #TODO:clear the drawingarea (which is already double buffered IIUC)
+
     draw_grid()
     @balls.each_with_index { |ball,idx|
       ball.draw self
     }
+  end
+
+  def ball_expired(ball)
+    @expired_balls << ball
   end
 end
 
@@ -117,15 +139,15 @@ class Ball
   end
   
   def draw(ctx)
-    gridpos1=ctx.gridpos(@x, @y)
-    gridpos2=ctx.gridpos(@x + @direction[0], @y + @direction[1])
+    gridcenter1=ctx.gridcenter(@x, @y)
+    gridcenter2=ctx.gridcenter(@x + @direction[0], @y + @direction[1])
     @direction == [1,0] and imgname="yspin%02d"%(ctx.steps-@step)
     @direction == [-1,0] and imgname="yspin%02d"%(@step)
     @direction == [0,1] and imgname="xspin%02d"%(ctx.steps-@step)
     @direction == [0,-1] and imgname="xspin%02d"%(@step)
     pb=ctx.images[imgname]
-    gx = gridpos1[0] + (gridpos2[0]-gridpos1[0]) * @step/ctx.steps - pb.width/2
-    gy = gridpos1[1] + (gridpos2[1]-gridpos1[1]) * @step/ctx.steps - pb.height/2
+    gx = gridcenter1[0] + (gridcenter2[0]-gridcenter1[0]) * @step/ctx.steps - pb.width/2
+    gy = gridcenter1[1] + (gridcenter2[1]-gridcenter1[1]) * @step/ctx.steps - pb.height/2
     ctx.drawingarea.window.draw_pixbuf(ctx.gc, pb, 0, 0, gx, gy, -1, -1, Gdk::RGB::DITHER_NONE, -1, -1)
   end
 
@@ -136,13 +158,13 @@ class Ball
       @x+=@direction[0]
       @y+=@direction[1]
       puts "new ball position is #{@x},#{@y}"
-      #TODO:if the ball is on an arrow tile,
-      #change @direction
-    end
-  end
 
-  def keep?
-    return (@x>=0 and @y>=0 and @x<=10 and @y<=10)
+      if @x < 0 || @x >= ctx.gridwidth || @y < 0 || @y >= ctx.gridheight
+        ctx.ball_expired(self)
+      elsif ctx.grid[x][y]
+        @direction=ctx.grid[x][y]
+      end
+    end
   end
 
 end
